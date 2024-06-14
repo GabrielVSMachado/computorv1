a6fed05bb43bb3c66c16a7286ed69c5476a89a53
gabriel.machado <gabriel.machado@loft.com.br>
2024-06-14 17:28:08 -0300

    feat: parse polynomial as entities of Parcel data type

--- src/PolynomialTypes.hs
+++ src/PolynomialTypes.hs
@@ -1,4 +1,4 @@
-module PolynomialTypes (Parcel (Parcel)) where
+module PolynomialTypes (Parcel (Parcel), polynomialParse) where
 
 import Text.Read (readMaybe)
 import Utils (split, trimStrings)
@@ -8,6 +8,8 @@ type Degree = Maybe Int
 type Constant = Maybe Float
 
 data Parcel = Parcel Degree Constant deriving (Show)
+
+type Polynomial = [(Int, Float)]
 
 degreeParser :: String -> Degree
 degreeParser s
@@ -25,12 +27,34 @@ sortParcelString input
 
 -- TODO: Parse a full polinomyal like '5 * X^0 + 4 * X^1 - 9.3 * X^2';
 
--- TODO: Parse the received polynomial to resolved to a allowed format
--- ex.: 1/2 * X^1 + 5 * X^0 turn into 0.5 * X^1 + 5 * X^0
-
 instance Read Parcel where
   readsPrec _ input =
     let (first, second) = sortParcelString input
         constant = readMaybe first :: Constant
         degree = degreeParser second
      in [(Parcel degree constant, "")]
+
+fromParcel :: Parcel -> (Int, Float)
+fromParcel (Parcel (Just x) (Just y)) = (x, y)
+fromParcel (Parcel Nothing (Just x)) = (0, x)
+fromParcel _ = error "Invalid polynomial"
+
+
+parcelString :: String -> String
+parcelString [] = []
+parcelString _all@(x : xs)
+  | x == '-' = x : parcel xs
+  | x == '+' = ' ' : parcel xs
+  | otherwise = parcel _all
+  where
+    parcel = takeWhile (`notElem` "+-")
+
+parcels :: String -> [String]
+parcels [] = []
+parcels xs = parcel : parcels rest
+  where
+    parcel = parcelString xs
+    rest = drop (length parcel) xs
+
+polynomialParse :: String -> Polynomial
+polynomialParse = map (fromParcel . \x -> read x :: Parcel) . parcels
