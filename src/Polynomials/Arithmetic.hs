53de8911a3bfc378b2c7b8931ac909053acd722f
gabriel.machado <gabriel.machado@loft.com.br>
2024-06-23 16:55:15 -0300

    fix: wrong vector's addition with distinct number of columns (distinct degrees)

--- src/Polynomials/Arithmetic.hs
+++ src/Polynomials/Arithmetic.hs
@@ -1,15 +1,26 @@
-module Polynomials.Arithmetic (add) where
+module Polynomials.Arithmetic (add, addSymetric) where
 
 import Data.List (transpose)
 import Polynomials.Types (Polynomial)
 import Utils (quickSort)
 
+nullPolynomial :: Polynomial
+nullPolynomial = zip [0, 1 ..] [0, 0 ..]
+
+fillPolynomial :: Int -> Polynomial -> Polynomial
+fillPolynomial n p = leftPart ++ p
+ where
+  pDegrees = [x | (x, _) <- p]
+  leftPart = [(x1, x2) | (x1, x2) <- take n nullPolynomial, x1 `notElem` pDegrees]
+
 add :: Polynomial -> Polynomial -> Polynomial
-add px py = zip [0, 1 .. degree] (map sum $ transpose [cx, cy])
+add px py = zip [0, 1 .. higherDegree - 1] (map sum $ transpose [sx, sy])
+ where
+  higherDegree = maximum [x1 | (x1, _) <- px ++ py] + 1 -- first degree is zero
+  sx = [x | (_, x) <- quickSort (fillPolynomial higherDegree px)]
+  sy = [y | (_, y) <- quickSort (fillPolynomial higherDegree py)]
+
+addSymetric :: Polynomial -> Polynomial -> Polynomial
+addSymetric px py = add px spy
   where
-    lengthPx = length px
-    lengthPy =  length py
-    higher = max lengthPx lengthPy
-    degree = higher - 1
-    cx = replicate (higher - lengthPx) 0 ++ [x | (_, x) <- quickSort px]
-    cy = replicate (higher - lengthPy) 0 ++ [y | (_, y) <- quickSort py]
+    spy = map (\(y1, y2) -> (y1, -y2)) py
